a10942ed5f11bbd46453608ed2855bacaa6c21b9
rijalmumtaz <aularrijalalmumtaz@gmail.com>
2023-04-08 20:55:42 +0700

    Button Component Done

---
+++ elements/Button/index.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import propTypes from "prop-types";
+import { Link } from "next/link";
+
+export default function Button(props) {
+  const className = [props.className];
+  if (props.isPrimary)
+    className.push("font-medium rounded-lg font-rozha-one bg-blue-700");
+  if (props.isSmall) className.push("px-3 py-2 text-sm text-center");
+  if (props.isBase) className.push("text-sm px-5 py-2.5 text-center");
+  if (props.isLarge) className.push("px-5 py-3 text-base text-center");
+  if (props.isOutline)
+    className.push(
+      "font-medium rounded-lg text-sm font-rozha-one border border-blue-700"
+    );
+  if (props.hasShadow) className.push("drop-shadow-xl");
+
+  const onClick = () => {
+    if (props.onClick) props.onClick();
+  };
+
+  if (props.type === "link") {
+    if (props.isExternal) {
+      return (
+        <a
+          href={props.hre}
+          className={className.join(" ")}
+          style={props.style}
+          target={props.target === "_blank" ? "_blank" : undefined}
+          rel={props.target === "_blank" ? "noopener noreferrer" : undefined}
+        >
+          {props.children}
+        </a>
+      );
+    } else {
+      return (
+        <Link
+          href={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          onClick={onClick}
+        >
+          {props.children}
+        </Link>
+      );
+    }
+  }
+
+  return (
+    <button
+      className={className.join(" ")}
+      style={props.style}
+      onClick={onClick}
+    >
+      {props.children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  type: propTypes.oneOf(["button", "link"]),
+  onClick: propTypes.func,
+  className: propTypes.string,
+  href: propTypes.string,
+  isPrimary: propTypes.bool,
+  isSmall: propTypes.bool,
+  isBase: propTypes.bool,
+  isLarge: propTypes.bool,
+  isOutline: propTypes.bool,
+  isExternal: propTypes.bool,
+  hasShadow: propTypes.bool,
+};
