73736d20b91e22a2cb96699a483ffb9115c92cc3
rijalmumtaz <aularrijalalmumtaz@gmail.com>
2023-04-25 17:39:55 +0700

    Music Part Done

--- parts/Music.jsx
+++ parts/Music.jsx
@@ -1,29 +1,56 @@
-import Button from "@/elements/Button";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Howl, Howler } from "howler";
+import { RiMusicFill } from "react-icons/ri";
+
+import Button from "@/elements/Button";
 
 export default function Music() {
-  const sound = new Howl({
-    src: ["/audios/Rey Mbayang  Di Sepertiga Malam.mp3"],
-    // autoplay: true,
-    volume: 0.3,
-    onend: function () {
-      console.log("Finished!");
-    },
-  });
+  const [animation, setAnimation] = useState(true);
+  const [sound, setSound] = useState(
+    new Howl({
+      src: ["/audios/Rey Mbayang  Di Sepertiga Malam.mp3"],
+      // autoplay: true,
+      volume: 0.3,
+      html5: true,
+      loop: true,
+      onend: function () {
+        console.log("Finished!");
+      },
+    })
+  );
+
+  useEffect(() => {
+    sound.play();
+  }, []);
 
   const togglePlay = () => {
-    return sound.playing() ? sound.pause() : sound.play();
+    if (sound.playing()) {
+      sound.pause();
+      setAnimation(false);
+    } else {
+      sound.play();
+      setAnimation(true);
+    }
   };
+
   return (
     <div className="relative">
       <Button
+        id="audio"
         type="button"
         isBase
         isPrimary
-        className="bg-brown h-16 w-16 rounded-[50%] fixed z-20 bottom-5 right-5"
+        className={`fixed bg-brown h-14 w-14 rounded-[500%] z-20 bottom-5 right-5 shadow-lg ${
+          animation && "animate-spin"
+        }`}
         onClick={togglePlay}
-      ></Button>
+      >
+        <img
+          className="w-6 h-w-6"
+          src="/images/music_note.svg"
+          alt="music"
+        />
+      </Button>
     </div>
   );
 }
